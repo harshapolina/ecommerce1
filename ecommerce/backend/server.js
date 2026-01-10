0ea161a55ac2d2d7dcccf0df784fbc53eb33bc2a
Harsha Polina <harshapolina1@gmail.com>
2026-01-10 12:19:19 +0530

    updated

--- ecommerce/backend/server.js
+++ ecommerce/backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB
-connectDB();
+
 
 // Middleware
 app.use(cors());
@@ -28,5 +28,8 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+
   console.log(`Server running on port ${PORT}`);
+
 });
+connectDB();
